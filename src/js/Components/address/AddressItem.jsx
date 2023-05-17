0375c12c14fa71fad174fff628f6c7e4031b1e42
Vivek Ghanchi <vivekghanchi12345@gmail.com>
2023-05-17 17:19:20 +0530

    Handling of empty valaus

--- src/js/Components/address/AddressItem.jsx
+++ src/js/Components/address/AddressItem.jsx
@@ -5,9 +5,9 @@ const AddressItem = ({ item }) => {
         <div><b>{item.fullname}</b> <br /> </div>
         <div>{item.houseno} <br /> </div>
         <div>{item.area} <br /> </div>
-        <div>{item.city}, {item.state} {item.pincode}<br /> </div>
+        {item.city && item.state && item.pincode && <div>{item.city}, {item.state} {item.pincode}<br /> </div>}
         <div>{item.country} <br /> </div>
-        <div>Phone no: {item.mobile} <br /> </div>
+        {item.mobile && <div>Phone no: {item.mobile} <br /> </div>}
     </>
 }
 
