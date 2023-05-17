329edea6a53f9f6c18027533af73afaac74098f2
Vivek Ghanchi <vivekghanchi12345@gmail.com>
2023-05-17 16:37:06 +0530

    CSS Handling for TextField

--- src/js/Components/address/AddressAddEdit.jsx
+++ src/js/Components/address/AddressAddEdit.jsx
@@ -26,18 +26,20 @@ const AddressAddEdit = ({ saveNewAddress, item, isAddNew }) => {
     }
 
     return <>
-        <h3> {isAddNew ? "Add a new address" : "Edit your address"} </h3><br />
-        <TextField label={'Country'} value={'India'} name={"country"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Full name'} value={addNewAddress.fullname} name={"fullname"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Mobile Number'} value={addNewAddress.mobile} name={"mobile"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Pincode'} value={addNewAddress.pincode} name={"pincode"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Flat, House no., Building, Company, Apartment'} value={addNewAddress.houseno} name={"houseno"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Area, Street, Sector, Village'} value={addNewAddress.area} name={"area"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Landmark'} value={addNewAddress.landmark} name={"landmark"} onChangeInput={onChangeInput} /><br />
-        <TextField label={'Town/City'} value={addNewAddress.city} name={"city"} onChangeInput={onChangeInput} /> <br />
-        <TextField label={'State'} value={addNewAddress.state} name={"state"} onChangeInput={onChangeInput} />
-        <br />
-        <button onClick={saveAddress}>{isAddNew ? "Add Address" : "Save Changes"} </button>
+        <div className={"a-row"}>
+            <h3> {isAddNew ? "Add a new address" : "Edit your address"} </h3><br />
+            <TextField label={'Country'} value={'India'} name={"country"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Full name'} value={addNewAddress.fullname} name={"fullname"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Mobile Number'} value={addNewAddress.mobile} name={"mobile"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Pincode'} value={addNewAddress.pincode} name={"pincode"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Flat, House no., Building, Company, Apartment'} value={addNewAddress.houseno} name={"houseno"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Area, Street, Sector, Village'} value={addNewAddress.area} name={"area"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Landmark'} value={addNewAddress.landmark} name={"landmark"} onChangeInput={onChangeInput} /><br />
+            <TextField label={'Town/City'} value={addNewAddress.city} name={"city"} onChangeInput={onChangeInput} /> <br />
+            <TextField label={'State'} value={addNewAddress.state} name={"state"} onChangeInput={onChangeInput} />
+            <br />
+            <button onClick={saveAddress}>{isAddNew ? "Add Address" : "Save Changes"} </button>
+        </div>
     </>
 }
 
--- src/js/Components/core/TextField.jsx
+++ src/js/Components/core/TextField.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 const TextField = ({ name, value, onChangeInput, className, placeholder, disabled, onBlur, onKeyDown, maxLength, label }) => {
     return <>
-        <label>
-            <b>{label}</b>:
-            <input
-                value={value}
-                className={className}
-                placeholder={placeholder}
-                onChange={onChangeInput}
-                disabled={disabled}
-                onBlur={onBlur}
-                onKeyDown={onKeyDown}
-                type="text"
-                name={name}
+        <div className="a-section a-spacing-none adddress-ui-widgets-form-field-label-container">
+            <div className="a-section a-spacing-none aok-inline-block">
+                <label for="address-ui-widgets-enterAddressFullName" className="a-form-label address-ui-widgets-desktop-form-field-full-width a-nowrap">
+                    <span className="a-size-base">{label}
+                    </span>
+                </label>
+            </div>
+        </div>
+
+        <div class="a-section a-spacing-base adddress-ui-widgets-form-field-container">
+            <input type="text"
                 maxLength={maxLength}
-            />
-        </label>
+                id={name}
+                name={name}
+                spellcheck="false"
+                value={value}
+                onChange={onChangeInput}
+                className="a-input-text address-ui-widgets-desktop-form-field-full-width addrui-form-text-input" />
+        </div>
     </>
 }
 
